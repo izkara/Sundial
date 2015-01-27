92e4fd727178cb6cbf3cdff7f292bc36bbad3c04
izkara <yelizkaradayi@gmail.com>
2015-01-27 05:19:50 -0500

    Merge branch 'face' of https://github.com/izkara/Sundial into face

--- src/face.c
+++ src/face.c
@@ -17,22 +17,39 @@ static const GPathInfo HOUSE_PATH_POINTS = {
   }
 }; 
 
+static const GPathInfo GNOMON = {
+  // This is the amount of points
+  4,
+  // A path can be concave, but it should not twist on itself
+  // The points should be defined in clockwise order due to the rendering
+  // implementation. Counter-clockwise will work in older firmwares, but
+  // it is not officially supported
+  (GPoint []) {
+    {-5, 0}, {5, 0}, {5,-30}, {-5,- 30} 
+  }
+}; 
+
+static const GPathInfo TICK = {
+  // This is the amount of points
+  4,
+  // A path can be concave, but it should not twist on itself
+  // The points should be defined in clockwise order due to the rendering
+  // implementation. Counter-clockwise will work in older firmwares, but
+  // it is not officially supported
+  (GPoint []) {
+    {-8, -3}, {8, -3}, {8,3}, {-8,3}   
+  }
+}; 
+
 #define HRS 15
 
   
-static GPath *house_path;
-
+static GPath *tick_path;
+static GPath *gnomon_path;
 static GPoint ticks[HRS];
 static double thla[HRS];
 // static int thr[HRS];
 
-#define NUM_GRAPHIC_PATHS 1
-
-static GPath *graphic_paths[NUM_GRAPHIC_PATHS];
-
-static GPath *current_path = NULL;
-
-static int current_path_index = 0;
 
 static int path_angle = 0;
 
@@ -42,29 +59,41 @@ static bool outline_mode = false;
 
 // This is the layer update callback which is called on render updates
 static void path_layer_update_callback(Layer *me, GContext *ctx) {
-  (void)me;
+  int x = layer_get_frame(me).size.w/2;
+  int y = layer_get_frame(me).size.h/2;
 
+  (void)me;
+  
   // You can rotate the path before rendering
-  gpath_rotate_to(current_path, (TRIG_MAX_ANGLE / 360) * path_angle);
 
   // There are two ways you can draw a GPath: outline or filled
   // In this example, only one or the other is drawn, but you can draw
   // multiple instances of the same path filled or outline.
-  
-  graphics_context_set_stroke_color(ctx, GColorBlack);
-  graphics_context_set_fill_color(ctx, GColorBlack);  
+
   
   for (int i = 0; i < HRS; i++) {
-    graphics_draw_circle(ctx, ticks[i], 5);
+    graphics_draw_line(ctx, GPoint(ticks[i].x,ticks[i].y), GPoint(x,y));
+    graphics_draw_line(ctx, GPoint(ticks[i].x+1,ticks[i].y), GPoint(x,y));
+    graphics_draw_line(ctx, GPoint(ticks[i].x,ticks[i].y+1), GPoint(x,y));
+    graphics_draw_line(ctx, GPoint(ticks[i].x-1,ticks[i].y), GPoint(x,y));
+    graphics_draw_line(ctx, GPoint(ticks[i].x,ticks[i].y-1), GPoint(x,y));
   }
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_circle(ctx, GPoint(x,y), 55); 
+  
   if (outline_mode) {
     // draw outline uses the stroke color
+    
+    graphics_context_set_fill_color(ctx, GColorWhite);
+    gpath_draw_filled(ctx, gnomon_path);
     graphics_context_set_stroke_color(ctx, GColorBlack);
-    gpath_draw_outline(ctx, current_path);
+    gpath_draw_outline(ctx, gnomon_path);
   } else {
     // draw filled uses the fill color
     graphics_context_set_fill_color(ctx, GColorBlack);
-    gpath_draw_filled(ctx, current_path);
+    gpath_draw_filled(ctx, gnomon_path);
+    graphics_context_set_stroke_color(ctx, GColorBlack);
+    gpath_draw_outline(ctx, gnomon_path);
   }
 }
 
@@ -94,9 +123,6 @@ static void select_raw_up_handler(ClickRecognizerRef recognizer, void *context) 
   // Show the path filled
   outline_mode = false;
   // Cycle to the next path
-  current_path_index = (current_path_index+1) % NUM_GRAPHIC_PATHS;
-  current_path = graphic_paths[current_path_index];
-  layer_mark_dirty(path_layer);
 }
 
 static void config_provider(void *context) {
@@ -204,8 +230,8 @@ static void fill_ticks(GPoint cen){
 //      int32_t second_angle = TRIG_MAX_ANGLE * (t.tm_sec / 60);
      int32_t second_angle = TRIG_MAX_ANGLE + (TRIG_MAX_ANGLE * (thla[i]/360));
      // int32_t second_angle = (-TRIG_MAX_ANGLE/4) + (TRIG_MAX_ANGLE/2*i/(HRS-1));
-     int y = (-cos_lookup(second_angle) * (cen.x-9) / TRIG_MAX_RATIO) + cen.y;
-     int x = (sin_lookup(second_angle) * (cen.x-9) / TRIG_MAX_RATIO) + cen.x;
+     int y = (-cos_lookup(second_angle) * (cen.x-3) / TRIG_MAX_RATIO) + cen.y;
+     int x = (sin_lookup(second_angle) * (cen.x-3) / TRIG_MAX_RATIO) + cen.x;
      ticks[i] = GPoint(x, y);
   }
 }
@@ -226,35 +252,22 @@ static void init() {
   get_tick_data(40, 79, 80);
   fill_ticks(center);
   
+  tick_path = gpath_create(&TICK);
+  gnomon_path = gpath_create(&GNOMON);
+  
   path_layer = layer_create(bounds);
   layer_set_update_proc(path_layer, path_layer_update_callback);
   layer_add_child(window_layer, path_layer);
 
-  // Pass the corresponding GPathInfo to initialize a GPath
+
   
-  house_path = gpath_create(&HOUSE_PATH_POINTS);
-  
-//   infinity_path = gpath_create(&INFINITY_RECT_PATH_POINTS);
-
-  // This demo allows you to cycle paths in an array
-  // Try adding more GPaths to cycle through
-  // You'll need to define another GPathInfo
-  // Remember to update NUM_GRAPHIC_PATHS accordingly
-  graphic_paths[0] = house_path;
-//   graphic_paths[1] = infinity_path;
-
-  current_path = graphic_paths[0];
-
   // Move all paths to the center of the screen
-
-  for (int i = 0; i < NUM_GRAPHIC_PATHS; i++) {
-    gpath_move_to(graphic_paths[i], center);
-  }
+  gpath_move_to(gnomon_path, center);
 }
 
 static void deinit() {
-  gpath_destroy(house_path);
-//   gpath_destroy(infinity_path);
+  gpath_destroy(gnomon_path);
+  gpath_destroy(tick_path);
 
   layer_destroy(path_layer);
   window_destroy(window);
